430bbfaed36670cf26bef06770caab6b74539e6a
sweetcapucino <thuylinh271055@gmail.com>
2021-10-23 16:29:53 +0700

    Error Page (done)

--- src/components/Layout/error_page.js
+++ src/components/Layout/error_page.js
@@ -1,5 +1,15 @@
 import React from "react";
+import ErrorImage from "../../assets/Icons/404 Error.svg";
+import "../../style/error.css";
 
 export const ErrorPage = () => {
-  return <div>Error</div>;
-};
+  return (
+    <>
+      <div className="error">
+        <img className="error_image" src={ErrorImage} alt="404 error" />
+        <h2 className="error_msg">Uh oh! Looks like you got lost.</h2>
+        <button className="error_btn" onclick="">BACK TO PREVIOUS PAGE</button>
+      </div>
+    </>
+  )
+}
