450e108c424aba473e7af02091920422788c70ed
sweetcapucino <thuylinh271055@gmail.com>
2021-10-23 15:17:19 +0700

    about: privacy (done)

---
+++ src/components/About_Nav/about_privacy.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import "../../style/About/about.css";
+
+export const Privacy = () => {
+    return (
+        <div className="about_wrapper">
+            <h1 className="about_title">PRIVACY POLICY</h1>
+            <div className="about_content">
+                <div className="about_subwrapper">
+                    <p>This privacy policy sets out how Chito Stationery uses and protects any information that you give us 
+                        when you use this website. Chito Stationery is committed to ensuring that your privacy is protected. 
+                        Should we ask you to provide certain information by which you can be identified when using this website, 
+                        then you can be assured that it will only be used in accordance with this privacy statement. 
+                        It is important that you read this Policy so that you are fully aware of how and why we are using your data.</p>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">WHO WE ARE</h3>
+                    <p>The data controller who is responsible for your personal data is Chito Station, The Chocolate Factory, 
+                        2700 Latte Road, Robusta. If you have any questions or concerns relating to your personal data or this Policy, 
+                        please contact us via email (studio@chitostationery.com) or post (The Chocolate Factory, 2700 Latte Road, Robusta).</p>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">CHANGES TO THIS POLICY</h3>
+                    <p>This Policy was last updated on 1st October 2021. We may need to make updates or changes to this Policy from time to time. 
+                        We will inform you of any such changes. Please let us know if your personal data changes during your relationship with us 
+                        so that we can update our records.</p>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">WHAT WE COLLECT</h3>
+                    <p>We may collect, use, store and process the following types of personal data about you:</p>
+                    <p>— Identity Data includes your name, username (or similar identifier), title</p>
+                    <p>— Contact Data includes billing address, delivery address, email address and telephone numbers</p>
+                    <p>— Transaction Data includes details about payments to and from you and other details of products and services you have 
+                        purchased from us (this does not include Financial Data)</p>
+                    <p>— Technical Data includes internet protocol (IP) address, your login data, browser type and version, time zone 
+                        setting and location, browser plug-in types and versions, operating system and platform and other technology 
+                        on the devices you use to access our website</p>
+                    <p>— Profile Data includes your username and password, purchases or orders made by you, your interests, preferences, 
+                        feedback and survey responses</p>
+                    <p>— Usage Data includes information about how you use our website, products and services</p>
+                    <p>— Marketing and Communications Data includes your preferences in receiving marketing from us and our third parties and 
+                        your communication preferences
+                    </p>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">HOW WE COLLECT YOUR DATA</h3>
+                </div>
+                <div className="about_special_subwrapper">
+                    <h4>Direct interactions</h4>
+                    <p>You may give us your Identity and Contact Data by filling in forms or by corresponding with us by post, phone, email 
+                        or otherwise. This includes personal data you provide when you</p>
+                    <p>— purchase our products or services;</p>
+                    <p>— create an account on our website;</p>
+                    <p>— subscribe to our mailing list;</p>
+                    <p>— give us some feedback</p>
+                </div>
+                <div className="about_special_subwrapper">
+                    <h4>Automated technologies or interactions</h4>
+                    <p>As you interact with our website, we may automatically collect Technical Data and Usage Data about your equipment, 
+                        browsing actions and patterns. We collect this personal data by using cookies and other similar technologies</p>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">WHAT WE DO WITH THE INFORMATION WE GATHER</h3>
+                    <p>We will only use your personal data in order to:</p>
+                    <p>— fulfil an order you have placed with us</p>
+                    <p>— send you marketing materials via email (only if you have given your express permission for us to do so, see below)</p>
+                    <p>— if required by law to pass on your details to law enforcement</p>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">SECURITY</h3>
+                    <p>Although the transmission of data over the internet may be subject to intrusion, we protect your personal data by 
+                        ensuring we have implemented appropriate physical, electronic and procedural safeguards to avoid unauthorised access or 
+                        disclosure, accidental loss, use, alteration or destruction of such Information. Where we engage third-party contractors, 
+                        as far as is necessary, we may share Information in order for such parties to perform services on our behalf and in 
+                        accordance with enforceable confidentiality obligations. We have put in place procedures to deal with any suspected 
+                        personal data breach and will notify you and any applicable regulator of a breach where we are legally required to do so.</p>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">CONTROLLING YOUR PERSONAL INFORMATION</h3>
+                    <p>Please contact us by email, should you wish to:</p>
+                    <p>— Request access to your personal data</p>
+                    <p>— Request correction of your personal data</p>
+                    <p>— Request erasure of your personal data</p>
+                    <p>— Object to processing of your personal data</p>
+                    <p>— Withdraw consent to our use of your personal data</p>
+                    <div className="about_special_subwrapper">
+                        <p>We may need to request specific information from you to help us confirm your identity and ensure your right to 
+                            exercise any of these rights. We try to respond to all legitimate requests within one month.</p>
+                    </div>
+                    <div className="about_special_subwrapper">
+                        <p>We will only retain your personal data for as long as necessary to fulfil the purposes we collected it for, 
+                            including for the purposes of satisfying any legal, accounting, or reporting requirements. By law we have to keep 
+                            basic information about our customers (including Contact, Identity, Financial and Transaction Data) for six years after 
+                            they cease being customers for tax purposes.</p>
+                    </div>
+                    <div className="about_special_subwrapper">
+                        <p>In some circumstances we may anonymise your personal data (so that it can no longer be associated with you) 
+                            for research or statistical purposes in which case we may use this information indefinitely without further 
+                            notice to you.</p>
+                    </div>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">IP ADDRESS</h3>
+                    <p>We may collect information about your computer, including where available your IP address, operating system 
+                        and browser type, for system administration and to report aggregate information to our advertisers. 
+                        This is statistical data about our users’ browsing actions and patterns, and does not identify any individual.</p>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">LINKS TO OTHER WEBSITES</h3>
+                    <p>This Policy applies to our services only. The Site may contain links to third-party websites, plug-ins and applications 
+                        over which we have no control. Clicking on those links or enabling those connections may allow third parties to collect 
+                        or share data about you. We are not responsible for the privacy policies or practices of such other websites 
+                        and we encourage you to review the privacy policies, if any, on all such websites.</p>
+                </div>
+                <div className="about_subwrapper">
+                    <h3 className="about_subtitle">COOKIES</h3>
+                    <p>Cookies are small amounts of information which we may store on your computer. Cookies make it easier for you to log on to 
+                        and use the site during future visits. They also allow us to monitor website traffic and to personalise the content of 
+                        the Site for you. You may set up your computer to reject all or some cookies although, in that case, you may not be 
+                        able to use certain features on the Site. See our cookies page for further details.</p>
+                </div>
+                <div className="about_subwrapper">
+                    <h4>We reserve the right to modify this Policy from time to time.</h4>
+                </div>
+            </div>
+        </div>
+            //                  
+    )
+}
--- src/components/About_Nav/about_screen.js
+++ src/components/About_Nav/about_screen.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Delivery } from "./about_delivery"
 import { Terms } from "./about_terms"
+import { Privacy } from "./about_privacy"
 
 export const About = () => {
     return (
         <>
             <div className="about">
                 {/* <Delivery/> */}
-                <Terms/>
+                {/* <Terms/> */}
+                <Privacy/>
             </div>
         </>
     )
